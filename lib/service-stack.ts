a621c0a33a1749898d03224dcaa87266eb6f33f1
Łukasz Budnik <lukasz.budnik@gmail.com>
2021-07-06 13:15:10 +0000

    added authorizer with jwtIssuer and jwtAudience as stack parameters

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as ddb from "@aws-cdk/aws-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigateway from "@aws-cdk/aws-apigatewayv2";
+import { HttpJwtAuthorizer } from "@aws-cdk/aws-apigatewayv2-authorizers";
 import { LambdaToDynamoDB } from "@aws-solutions-constructs/aws-lambda-dynamodb";
 import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
 
@@ -12,6 +13,21 @@ export interface StackProps {
 export class Service extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: StackProps) {
     super(scope, id);
+
+    const jwtIssuer = new cdk.CfnParameter(this, "jwtIssuer", {
+      type: "String",
+      description: "The URL of JWT Issuer",
+    });
+
+    const jwtAudience = new cdk.CfnParameter(this, "jwtAudience", {
+      type: "CommaDelimitedList",
+      description: "The audience of the JWT token",
+    });
+
+    const authorizer = new HttpJwtAuthorizer({
+      jwtAudience: jwtAudience.valueAsList,
+      jwtIssuer: jwtIssuer.valueAsString,
+    });
 
     const createItem = new LambdaToDynamoDB(this, "create-item", {
       lambdaFunctionProps: {
@@ -35,6 +51,11 @@ export class Service extends cdk.Stack {
 
     const httpApi = new apigateway.HttpApi(this, "http-api-service");
 
+    httpApi.addStage("dev", {
+      stageName: "dev",
+      autoDeploy: true,
+    });
+
     httpApi.addRoutes({
       path: "/items",
       methods: [apigateway.HttpMethod.GET],
@@ -49,6 +70,7 @@ export class Service extends cdk.Stack {
       integration: new LambdaProxyIntegration({
         handler: createItem.lambdaFunction,
       }),
+      authorizer: authorizer,
     });
   }
 }
